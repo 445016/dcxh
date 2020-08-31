489f5502e111462b443a7cb0c4fc0dfbf96d6760
Kevin Zhao <follow_kevin@126.com>
2020-08-31 23:43:57 +0800

    init code

---
+++ Modules/Seller/Conf/db.php
@@ -0,0 +1,13 @@
+<?php
+return array(
+//源码来自动能源码dghoyo-com
+'DB_TYPE'   => 'mysql', // 数据库类型
+'DB_HOST'   => '127.0.0.1', // 服务器地址
+'DB_NAME'   => 'dcxh', // 数据库名
+'DB_USER'   => 'root', // 用户名
+'DB_PWD'    => '',  // 密码
+'DB_PORT'   => '3306', // 端口
+'DB_PREFIX' => 'oscshop_', // 数据库表前缀
+
+);
+?>
